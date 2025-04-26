d6a9187ef79632abe4974db6ef52dfb3d731cf41
pramitawndr <mitacansu@gmail.com>
2025-04-26 23:49:11 +0800

    Update User

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,14 +1,19 @@
 package Model;
 
-public abstract class User {
-    protected String username;
-    protected String password;
+public class User {
+    private String username;
+    private String password;
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
-    public String getUsername() { return username; }
-    public String getPassword() { return password; }
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
