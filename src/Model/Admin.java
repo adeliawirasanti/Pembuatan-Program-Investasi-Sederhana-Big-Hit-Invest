1bce4451493464810cf4e4220d0f1d9a19e2fea3
Adelia Wirasanti <adeliawirasanti4@gmail.com>
2025-04-29 09:05:47 +0800

    Update Admin.java

--- src/Model/Admin.java
+++ src/Model/Admin.java
@@ -1,6 +1,7 @@
 package Model;
 
 public class Admin extends User {
+    // Memanggil konstruktor User untuk menginisialisasi username dan password. 
     public Admin(String username, String password) {
         super(username, password);
     }
